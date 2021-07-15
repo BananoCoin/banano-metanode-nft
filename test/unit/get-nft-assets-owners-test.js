c4b2bb615e4e4ca084385550c5726a5c9fbb1dd2
coranos <coranos@users.noreply.github.com>
2021-07-14 21:33:48 -0500

    added get-nft-assets-owners test coverage

--- test/unit/get-nft-assets-owners-test.js
+++ test/unit/get-nft-assets-owners-test.js
@@ -15,6 +15,10 @@ const goodIpfsCid = 'QmQJXwo7Ee1cgP2QVRMQGrgz29knQrUMfciq2wQWAvdzzS';
 const badContentTypeIpfsCid = 'QmQJXwo7Ee1cgP2QVRMQGrgz29knQrUMfciq2wQWABADCT';
 const badTimeoutIpfsCid = 'QmQJXwo7Ee1cgP2QVRMQGrgz29knQrUMfciq2wQBADTIME';
 const badUnknownIpfsCid = 'QmQJXwo7Ee1cgP2QVRMQGrgz29knQrUMfciqBADUNKNOWN';
+const badJsonIpfsCid = 'QmQJXwo7Ee1cgP2QVRMQGrgz29knQrUMfciq2wQBADJSON';
+const badMissingJsonIpfsCid = 'QmQJXwo7Ee1cgP2QVRMQGrgz29knQrUBAD2MISSINGJSON';
+const badJsonBase58Cid = 'QmQJXwo7Ee1cgP2QVRMQGrgz29knQrUMfBADJSONBASE58';
+const badJsonBase58ShortCid = 'QmQJXwo7Ee1cgP2QVRMQGrgz29knBADJSONBASE58SHORT';
 const goodHead = '0000000000000000000000000000000000000000000000000000000000000000';
 const goodLink = '0000000000000000000000000000000000000000000000000000000000000001';
 const goodOwner4link = '0000000000000000000000000000000000000000000000000000000000000002';
@@ -155,6 +159,100 @@ describe(actionUtil.ACTION, () => {
             });
           });
         }
+        if (resource == `${config.ipfsApiUrl}/${badJsonIpfsCid}`) {
+          return new Promise(async (resolve) => {
+            resolve({
+              status: 200,
+              headers: {
+                get: (header) => {
+                  if (header == 'content-type') {
+                    return 'application/json';
+                  }
+                },
+              },
+              json: () => {
+                return {
+                  'command': 'mint_nft',
+                  'version': '',
+                  'title': '',
+                  'issuer': '',
+                  'max_supply': '',
+                  'ipfs_cid': '',
+                  'mint_previous': '',
+                };
+              },
+            });
+          });
+        }
+        if (resource == `${config.ipfsApiUrl}/${badMissingJsonIpfsCid}`) {
+          return new Promise(async (resolve) => {
+            resolve({
+              status: 200,
+              headers: {
+                get: (header) => {
+                  if (header == 'content-type') {
+                    return 'application/json';
+                  }
+                },
+              },
+              json: () => {
+                return {
+                };
+              },
+            });
+          });
+        }
+        if (resource == `${config.ipfsApiUrl}/${badJsonBase58Cid}`) {
+          return new Promise(async (resolve) => {
+            resolve({
+              status: 200,
+              headers: {
+                get: (header) => {
+                  if (header == 'content-type') {
+                    return 'application/json';
+                  }
+                },
+              },
+              json: () => {
+                return {
+                  'command': 'mint_nft',
+                  'version': '',
+                  'title': '',
+                  'issuer': '',
+                  'max_supply': '1',
+                  'ipfs_cid': 'Qm#',
+                  'mint_previous': 'AB',
+                };
+              },
+            });
+          });
+        }
+        if (resource == `${config.ipfsApiUrl}/${badJsonBase58ShortCid}`) {
+          return new Promise(async (resolve) => {
+            resolve({
+              status: 200,
+              headers: {
+                get: (header) => {
+                  if (header == 'content-type') {
+                    return 'application/json';
+                  }
+                },
+              },
+              json: () => {
+                return {
+                  'command': 'mint_nft',
+                  'version': '',
+                  'title': '',
+                  'issuer': '',
+                  'max_supply': '1',
+                  'ipfs_cid': 'QmQJXwo7Ee1cgP2QVRMQGrgz29knQrUMfciq2wQWAvdzz',
+                  'mint_previous': 'AB',
+                };
+              },
+            });
+          });
+        }
+
         if (resource == config.bananodeApiUrl) {
           const body = JSON.parse(options.body);
           if (body.action == 'account_history') {
@@ -373,6 +471,130 @@ describe(actionUtil.ACTION, () => {
       ],
       'ipfs_cid': badUnknownIpfsCid,
       'status': 451,
+      'success': false,
+    };
+    loggingUtil.debug('actualResponse', actualResponse);
+    loggingUtil.debug('expectedResponse', expectedResponse);
+    expect(actualResponse).to.deep.equal(expectedResponse);
+  });
+  it('get status 200 badJsonIpfsCid', async () => {
+    const context = getContext();
+    let actualResponse;
+    try {
+      actualResponse = await getResponse(context, badJsonIpfsCid);
+    } catch (error) {
+      loggingUtil.trace(error);
+    }
+    const expectedResponse = {
+      'content_type': 'application/json',
+      'errors': [
+        'max_supply:\'\' not an integer',
+        'ipfs_cid:\'\' not Qm+base58',
+        'mint_previous:\'\' not 64 hex characters',
+      ],
+      'ipfs_cid': badJsonIpfsCid,
+      'json': {
+        'command': 'mint_nft',
+        'ipfs_cid': '',
+        'issuer': '',
+        'max_supply': '',
+        'mint_previous': '',
+        'title': '',
+        'version': '',
+      },
+      'status': 200,
+      'success': false,
+    };
+    loggingUtil.debug('actualResponse', actualResponse);
+    loggingUtil.debug('expectedResponse', expectedResponse);
+    expect(actualResponse).to.deep.equal(expectedResponse);
+  });
+  it('get status 200 badMissingJsonIpfsCid', async () => {
+    const context = getContext();
+    let actualResponse;
+    try {
+      actualResponse = await getResponse(context, badMissingJsonIpfsCid);
+    } catch (error) {
+      loggingUtil.trace(error);
+    }
+    const expectedResponse = {
+      'content_type': 'application/json',
+      'errors': [
+        'command:\'undefined\' !== \'mint_nft\'',
+        'version undefined',
+        'title undefined',
+        'issuer undefined',
+        'max_supply undefined',
+        'ipfs_cid undefined',
+        'mint_previous undefined',
+      ],
+      'ipfs_cid': badMissingJsonIpfsCid,
+      'json': {},
+      'status': 200,
+      'success': false,
+    };
+    loggingUtil.debug('actualResponse', actualResponse);
+    loggingUtil.debug('expectedResponse', expectedResponse);
+    expect(actualResponse).to.deep.equal(expectedResponse);
+  });
+  it('get status 200 badJsonBase58Cid', async () => {
+    const context = getContext();
+    let actualResponse;
+    try {
+      actualResponse = await getResponse(context, badJsonBase58Cid);
+    } catch (error) {
+      loggingUtil.trace(error);
+    }
+    const expectedResponse = {
+      'content_type': 'application/json',
+      'errors': [
+        'ipfs_cid:\'Qm#\' not Qm+base58',
+        'mint_previous:\'AB\' not 64 hex characters',
+      ],
+      'ipfs_cid': badJsonBase58Cid,
+      'json': {
+        'command': 'mint_nft',
+        'ipfs_cid': 'Qm#',
+        'issuer': '',
+        'max_supply': '1',
+        'mint_previous': 'AB',
+        'title': '',
+        'version': '',
+      },
+      'status': 200,
+      'success': false,
+    };
+    loggingUtil.debug('actualResponse', actualResponse);
+    loggingUtil.debug('expectedResponse', expectedResponse);
+    expect(actualResponse).to.deep.equal(expectedResponse);
+  });
+  it('get status 200 badJsonBase58ShortCid', async () => {
+    const context = getContext();
+    let actualResponse;
+    try {
+      actualResponse = await getResponse(context, badJsonBase58ShortCid);
+    } catch (error) {
+      loggingUtil.trace(error);
+    }
+    const expectedResponse = {
+      'content_type': 'application/json',
+      'errors': [
+        'ipfs_cid_hex:\'500080c4b27277b22c373e4f0dbbc86091707c6cc1745a1b3efcf66664bbde9ceb\' not 64 characters after prefix 1220, 66',
+        'mint_previous:\'AB\' not 64 hex characters',
+      ],
+      'ipfs_cid': badJsonBase58ShortCid,
+      'json': {
+        'command': 'mint_nft',
+        'ipfs_cid': 'QmQJXwo7Ee1cgP2QVRMQGrgz29knQrUMfciq2wQWAvdzz',
+        'ipfs_cid_hex': '500080c4b27277b22c373e4f0dbbc86091707c6cc1745a1b3efcf66664bbde9ceb',
+        'ipfs_cid_hex_base58': 'QmQJXwo7Ee1cgP2QVRMQGrgz29knQrUMfciq2wQWAvdzz',
+        'issuer': '',
+        'max_supply': '1',
+        'mint_previous': 'AB',
+        'title': '',
+        'version': '',
+      },
+      'status': 200,
       'success': false,
     };
     loggingUtil.debug('actualResponse', actualResponse);
